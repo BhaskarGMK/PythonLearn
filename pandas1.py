2536136a6fe3b47f9ba753928c89347c8c4521da
Bhaskar Gottemukkula <kharhanji@gmail.com>
2018-05-31 17:07:17 -0500

    merging & joining

--- pandas1.py
+++ pandas1.py
@@ -1,4 +1,21 @@
 import pandas as pd
 xyz = {'day' :[1,2,3,4,5,6], 'visited':[12,23,42,11,13,34], 'bounse':[1,4,2,1,4,5]}
-df = pd.DataFrame(xyz)
-print(df)
+# converting a dictionary into a dataframe
+df1 = pd.DataFrame(xyz)
+df3 = list(xyz.items())
+
+print(df3)
+print(df1.head(2))
+
+abc = {'day' :[7,8,9,10,11], 'visited':[22,33,44,41,23,14], 'bounse':[3,5,2,7,4,9]}
+df4 = list(abc.items())
+x = pd.DataFrame({'day' :[1,2,3,4,5,6], 'visited':[12,23,42,11,13,34], 'bounse':[1,4,2,1,4,5]}, index = [1,2,3,4,5,6])
+# df2 = pd.DataFrame(abc)
+a = pd.DataFrame({'day' :[1,2,3,4,5,7], 'visited':[22,33,44,41,23,14], 'bounse':[3,5,2,7,4,9]}, index = [7,8,9,10,11,12])
+
+df5 = pd.merge(x,a, on='day')
+
+print(df5)
+
+
+
--- pandas2.py
+++ pandas2.py
@@ -1,1 +1,30 @@
 import pandas as pd
+import matplotlib.pyplot as mp
+from matplotlib import style
+style.use("fivethirtyeight")
+# this is about joining
+x = pd.DataFrame({'visited':[12,23,42,11,13,34], 'bounse':[1,4,2,1,4,5], 'us':[12,23,34,12,22,45]}, index = [1,2,3,4,5,6])
+# df2 = pd.DataFrame(abc)
+a = pd.DataFrame({'visited':[22,33,44,41,23,14], 'bounse':[3,5,2,7,4,9], 'india':[12,23,34,12,22,45]}, index = [7,8,9,10,11,12])
+'''
+joining = a.join(x)
+
+# print(joining)
+
+df1 = pd.DataFrame({'casino':[1,2,3,4,5], 'visit':[20,40,50,24,35], 'left':[5,2,3,5,6]})
+df1.set_index('casino', inplace=True)
+print(df1)
+
+df1.plot()
+mp.show()
+df1 = df1.rename(columns = {"visit":"biscuits"})
+print(df1)
+
+
+concat = pd.concat([x,a], sort=True)
+print(concat)
+'''
+
+honey = pd.read_csv('honeyproduction.csv', index_col=0)
+print(honey)
+honey.to_html('honey.html')
